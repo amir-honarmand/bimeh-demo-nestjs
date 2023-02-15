e39e90465fa62d3239133d8c1fc8c983dfadd54f
amir honarmand <amir.honarmand.zade@gmail.com>
2023-02-15 20:57:28 +0330

    added checkNumber method in user service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,7 +4,7 @@ import { ValidateUserDto } from './dto/auth.dto';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UserRepository } from './repository/user.repository';
-import {validate, generate} from '../utils/password';
+import { validate, generate } from '../utils/password';
 import { JwtService } from '@nestjs/jwt';
 import { checkImageUpload } from 'src/utils/check-image-upload';
 
@@ -14,19 +14,19 @@ export class UsersService {
     private readonly UserRepository: UserRepository,
     private jwtService: JwtService,
 
-  ){}
+  ) { }
 
   async login(validateUserDto: ValidateUserDto): Promise<object> {
     const email: string = validateUserDto.email.toLowerCase();
     const password: string = validateUserDto.password;
-    
+
     const user = await this.UserRepository.findUser(email);
-    if(user.isBlock){
+    if (user.isBlock) {
       throw new ForbiddenException('you are blocked!');
     };
-    
+
     const checkPassword = await validate(password, user.salt, user.password);
-    if(!checkPassword){
+    if (!checkPassword) {
       throw new ServiceUnavailableException('email or password incorrect!');
     };
 
@@ -34,10 +34,10 @@ export class UsersService {
 
     return { token, userId: user.id, hasRegistered: true, /* role: user.role */ };
   }
-  
+
   async createUser(createUserDto: CreateUserDto): Promise<boolean> {
     // check mobile numbers validation
-    
+
     createUserDto = checkImageUpload(createUserDto, undefined, 'users');
 
     const passwordHash = await generate(createUserDto.password);
@@ -55,12 +55,12 @@ export class UsersService {
 
   async updateUserWallet(id: string, updateUserDto: UpdateUserDto): Promise<boolean> {
     // persian numbers check
-    
-    const amount: number = Number(updateUserDto.wallet);
-    if(!amount || isNaN(amount)) throw new InternalServerErrorException('amount is NaN!');
-    
+    const amount = this.checkNumbers(updateUserDto.wallet);
+
+    if (!amount || isNaN(amount)) throw new InternalServerErrorException('amount is Not a number!');
+
     await this.UserRepository.updateUserWallet(id, amount);
-    
+
     return true;
   }
 
@@ -75,5 +75,32 @@ export class UsersService {
 
   async validateUser(payload: any): Promise<any> {
     return await this.UserRepository.findUser(payload?.id);
-}
+  }
+
+  private checkNumbers(inputNumber: string): number {
+    const persianNumbers: string[] = ['۰','۱','۲','۳','۴','۵','۶','۷','۸','۹'];
+    const enNumbers: string[] = ['0','1','2','3','4','5','6','7','8','9'];
+    let newNumber: string = '';
+
+    const splitNumbers = inputNumber.split('');
+    const splitNumbersLength = splitNumbers.length;
+
+    for(let i = 0; i < splitNumbersLength; i++){
+      const num = splitNumbers[i];
+
+      if(num === '.'){
+        newNumber += num;
+        continue;
+      };
+
+      const numIndex = persianNumbers.findIndex(e => e === num);
+      if(numIndex !== -1){
+        newNumber += enNumbers.find((e, i)=> i === numIndex);
+      }else{
+        newNumber += num;
+      }
+    }
+
+    return +newNumber;
+  }
 }
